bd866c7eb85dcfb606d688345e604f170a22a4e7
Pratham-Mishra04 <prathamm0409@gmail.com>
2024-06-14 11:25:00 +0530

    feat: dyte client integrated for meetings

--- src/pages/organisation/meetings/live.tsx
+++ src/pages/organisation/meetings/live.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import Meeting from '@/screens/organisation/meeting';
+import Toaster from '@/utils/toaster';
+import { DyteProvider, useDyteClient } from '@dytesdk/react-web-core';
+import { useEffect } from 'react';
 
-const Meeting = () => {
-  return <div>Meeting</div>;
-};
+export default function App() {
+  const [meeting, initMeeting] = useDyteClient();
 
-export default Meeting;
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('mid');
+
+    if (!token || token == '') {
+      Toaster.error('Invalid session');
+      return;
+    }
+    initMeeting({
+      authToken: token,
+      defaults: {
+        audio: false,
+        video: false,
+      },
+    });
+  }, []);
+
+  return (
+    <DyteProvider value={meeting}>
+      <Meeting />
+    </DyteProvider>
+  );
+}
