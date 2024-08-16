131a730c88b1d6c8b3f6d09d27ed8eb8e0e5ddf7
Pratham-Mishra04 <prathamm0409@gmail.com>
2024-08-16 16:47:39 +0530

    feat: index redirection fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import nookies from 'nookies';
 import { GetServerSidePropsContext } from 'next/types';
 
 const Index = () => {
@@ -9,21 +8,11 @@ const Index = () => {
 export default Index;
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-  const token = nookies.get(context).token;
-  if (!token) {
-    return {
-      redirect: {
-        permanent: true,
-        destination: '/login',
-      },
-      props: {},
-    };
-  }
   return {
     redirect: {
       permanent: true,
       destination: '/home',
     },
-    props: { token },
+    props: {},
   };
 };
