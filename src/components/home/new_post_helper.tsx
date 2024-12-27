90ea72f3c86e65dd63fb31e95d9b814c59ee96e7
Pratham-Mishra04 <prathamm0409@gmail.com>
2024-12-27 23:50:32 +0530

    feat: post helper ui fixes

--- src/components/home/new_post_helper.tsx
+++ src/components/home/new_post_helper.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Info } from '@phosphor-icons/react';
-import Editor from '@/components/editor';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
 interface Props {
   smallScreen?: boolean;
 }
-
-const content =
-  '<p><code>#</code> <code>##</code> <code>###</code> <mark>Headings</mark></p><p><strong>bold </strong><code>ctrl+b</code> <code>__bold__</code></p><p><em>italic </em><code>ctrl+i</code> <code>_italic_</code></p><p><u>undeline</u> <code>ctrl+u</code></p><p><a target="_blank" rel="noopener noreferrer nofollow" class="" href="https://interactnow.in/home">link</a> <code>ctrl+l</code> <mark>paste a link</mark></p><ul><li><p><code>- </code><mark>Unordered List</mark></p></li></ul><ol><li><p><code>1. </code><mark>Ordered List</mark></p></li></ol><blockquote><p><code>&gt; </code><mark>Quotes</mark></p></blockquote><p><span data-type="mention" class="mention" data-mention-id="f5958e3b-51bd-47db-bf2e-247bf789a6d4" data-mention-category="users" data-mention-label="johndoe" data-mention-href="/users/johndoe">@johndoe</span> <mark>mention</mark></p><p><code>==mark==</code> <mark>marks</mark></p><p><code>[]</code> <mark>task</mark></p><ul data-type="taskList"><li data-checked="true" data-type="taskItem"><label><input type="checkbox" checked="checked"><span></span></label><div><p>Completed</p></div></li></ul><p><code>---</code> <mark>break</mark></p><hr><p><code>ctrl+z</code> <code>ctrl+y</code> <mark>undo &amp; redo</mark></p><p>and more….</p><p></p>';
 
 const NewPostHelper = ({ smallScreen }: Props) => {
   return (
@@ -16,10 +12,95 @@ const NewPostHelper = ({ smallScreen }: Props) => {
       <PopoverTrigger>
         <Info size={24} />
       </PopoverTrigger>
-      <PopoverContent>
+      <PopoverContent className="w-[420px] max-h-[520px] overflow-y-auto thin_scrollbar shadow-lg dark:shadow-neutral-900">
         <div className={`space-y-4 ${smallScreen ? 'text-xs' : 'text-sm'}`}>
-          <div className="text-center font-medium">Make your post more interesting!</div>
-          <Editor editable={false} content={content} className="w-full h-fit" />
+          <div className="text-center font-medium">Tips to make your post more interesting!</div>
+          <div className="w-full h-fit rounded-md grid grid-cols-2">
+            <div className="space-y-4">
+              <div className="space-y-1">
+                <h2 className="font-semibold mb-2 underline underline-offset-2">Headings</h2>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">#</code> → H1
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">##</code> → H2
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">###</code> → H3
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h2 className="font-semibold mb-2 underline underline-offset-2">Lists</h2>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">-</code> → Bullet list
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">1.</code> → Numbered list
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h2 className="font-semibold mb-2 underline underline-offset-2">Extras</h2>
+                <div className="flex items-center gap-1">
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">&gt;</code> →{' '}
+                  <div className="w-fit px-1 rounded-md border-dotted border-[1px] border-black dark:border-white">
+                    Quote
+                  </div>
+                </div>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">@search</code> → Mention
+                </p>
+                <div className="flex items-center gap-1">
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">==mark==</code> →{' '}
+                  <div className="w-fit px-1 bg-primary_text rounded-md">Highlight</div>
+                </div>
+                <div className="flex items-center gap-1">
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">[ ]</code> →{' '}
+                  <div className="flex-center gap-1">
+                    <input type="checkbox" />
+                    <div>Checkbox</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-4">
+              <div className="space-y-1">
+                <h2 className="font-semibold mb-2 underline underline-offset-2">Formatting</h2>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">ctrl+b</code> /{' '}
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">__bold__</code> → <strong>Bold</strong>
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">ctrl+i</code> {' / '}
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">_italic_</code> → <i>Italic</i>
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">ctrl+u</code> → <u>Underline</u>
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">ctrl+l</code> →{' '}
+                  <a href="#" className="text-blue-500 underline">
+                    Link
+                  </a>
+                </p>
+              </div>
+
+              <div className="space-y-1">
+                <h2 className="font-semibold mb-2 underline underline-offset-2">Other Actions</h2>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">---</code> → Divider
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">ctrl+z</code> → Undo
+                </p>
+                <p>
+                  <code className="bg-gray-200 text-black px-1 py-0.5 rounded">ctrl+y</code> → Redo
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* <Editor editable={false} content={content} className="w-full h-fit" /> */}
         </div>
       </PopoverContent>
     </Popover>
