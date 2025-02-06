869701b1fbc57629b4bc5e24010e42aa38fb6ddd
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2025-02-06 17:44:56 +0530

    fix: edit-links

--- src/components/form/links.tsx
+++ src/components/form/links.tsx
@@ -4,7 +4,7 @@ import LinksField from '../utils/edit_links';
 interface Props {
   label?: string;
   links: string[];
-  setLinks: React.Dispatch<React.SetStateAction<string[]>> | ((val: string[]) => void);
+  setLinks: React.Dispatch<React.SetStateAction<string[]>>;
   maxLinks: number;
   required?: boolean;
 }
